224a13dc4cf3a4048549fcd94248ed04d45946ac
jufj <juranfuture@gmail.com>
2026-01-05 17:46:18 +0800

    update

--- src/stores/pizzas.js
+++ src/stores/pizzas.js
@@ -8,7 +8,7 @@ export const usePizzasStore = defineStore("pizzas", () => {
   const fetchPizzas = async () => {
     try {
       const response = await axios.get('http://localhost:8700/pizzas/');
-      pizzas.value = response.data.data;
+      pizzas.value = response.data?.data;
     } catch (error) {
       console.error('There was an error fetching the pizzas!', error);
     }
