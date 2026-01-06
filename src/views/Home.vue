11a70fdd1ae7896a96b03445c41b9482c692e25a
JuRan <juranfuture@gmail.com>
2026-01-06 22:10:12 +0800

    feat: onBeforeRouteLeave

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,11 @@
 <script setup>
-  import NavBar from '@/components/NavBar.vue';
+import NavBar from '@/components/NavBar.vue';
+import { onBeforeRouteLeave } from 'vue-router';
+
+onBeforeRouteLeave((to, from, next) => {
+  const answer = window.confirm("Are you sure you want to leave?")
+  next(!!answer)
+})
 </script>
 <template>
   <NavBar />
