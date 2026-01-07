7715fdfb533d699c26f5165a3f914ce9a9ab14a9
jufj <juranfuture@gmail.com>
2026-01-07 17:56:19 +0800

    Using the Cart Store in a Component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
   import { RouterView } from 'vue-router';
+  import Cart from './views/Cart.vue';
 </script>
 
 <template>
+  <Cart/>
   <RouterView/>
 </template>
---
+++ src/views/Cart.vue
@@ -0,0 +1,17 @@
+<script setup>
+ import { useCartStore } from '@/stores/cart';
+ const cartStore = useCartStore()
+</script>
+
+<template>
+  <div class="cart">
+    <span class="cart_total">Cart: {{ cartStore.total }}</span>
+  </div>
+</template>
+
+<style scoped>
+  .cart_total{
+    cursor: pointer;
+    text-decoration: underline;
+  }
+</style>
